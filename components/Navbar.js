3f7ce5b89a5e1e2da0b8a1b69ac82372203defdc
ayham-kattan <ayhamkattan1@gmail.com>
2022-03-17 17:11:43 +0200

    Link_navbar

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+function Navbar() {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/About">About</Link>
+      <Link href="/Contact">Contact</Link>
+    </div>
+  );
+}
+
+export default Navbar
